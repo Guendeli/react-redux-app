8d58c94bd22580b7152597d67c3e8ea824868c3f
GUENDELI Omar <guendeliomar@gmail.com>
2018-01-31 17:50:13 +0000

    Layout Animations implemented

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,56 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableWithoutFeedback, View, NativeModules, LayoutAnimation} from 'react-native';
+import {connect} from 'react-redux';
+
 import {CardItem} from './common';
+import * as actions from '../actions';
+
+// Required for android to support LayoutAnimations
+const { UIManager } = NativeModules;
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 class ListItem extends Component {
+
+    componentWillUpdate(){
+        LayoutAnimation.spring();
+    }
+
+    renderDesc(){
+        const  { library, expanded} = this.props;
+
+        if(expanded){
+            return(
+                <CardItem>
+                    <Text
+                        style={styles.descStyle}
+                    >
+                        {library.description}
+                    </Text>
+                </CardItem>
+            );
+        }
+    }
+
     render(){
         const {library} = this.props;
+        const {id, title} = this.props.library;
 
         return(
-            <CardItem>
-                <Text
-                style={styles.titleStyle}
-                >
-                    {library.title}
-                </Text>
-            </CardItem>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id) }
+            >
+                <View>
+                    <CardItem>
+                        <Text
+                        style={styles.titleStyle}
+                        >
+                            {title}
+                        </Text>
+                    </CardItem>
+                    {this.renderDesc()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }    
@@ -22,6 +59,19 @@ const styles = {
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15
+    },
+    descStyle: {
+        flex: 1,
+        paddingLeft: 2,
+        paddingRight: 2
     }
+
 }
-export default ListItem;
+
+const mapStateToProps = (state, ownProps) => {
+
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+    return { expanded };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
